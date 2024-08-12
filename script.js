228a7fd0f6179aa950fac2471416733f6f8c9bd9
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-13 00:55:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+ let panels = document.querySelectorAll(".panel")
+
+     panels.forEach( (panel) => {
+          panel.addEventListener("click" , () => {
+               removeAllClasses()
+               panel.classList.add("active")
+          })
+     })
+
+     function removeAllClasses(){
+         panels.forEach((panel) => {
+               panel.addEventListener("click", () => {
+                      panel.classList.remove("active")
+               })
+         }) 
+     }
